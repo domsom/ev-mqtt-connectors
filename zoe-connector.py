34058d88567eb3fb47b55e5dcae56298528a6eb8
Dominik Sommer <dominik@sommer.name>
2023-11-12 16:13:24 +0100

    Decouple updating values from MQTT publishing and increase frequency of MQTT publishing so that consumers (such as the PowerBrain) have the impression that the info stream is active

--- zoe-connector.py
+++ zoe-connector.py
@@ -10,13 +10,18 @@ from pyze.api import Gigya, Kamereon, Vehicle
 from config import *
 from secrets import *
 
-
+# All values in seconds
 FREQUENCY_ACTIVE = 600
 FREQUENCY_INACTIVE = 3600
+MQTT_FREQUENCY = 30
 EXCEPTION_DELAY = 300
 
 _ACTIVE_STATES = [0.3, 0.4, 1.0] # see https://github.com/jamesremuscat/pyze/blob/c359492287ce1a5462b8b3e7ddca11919bbf04a4/src/pyze/api/kamereon.py#L370
 
+soc = None
+remaining_range = None
+charging_status = None
+plug_status = None
 
 def getSocRange(gigya):
 
@@ -29,19 +34,14 @@ def getSocRange(gigya):
     charging_status = b['chargingStatus']
     plug_status = b['plugStatus']
 
-    logging.info("Zoe API: soc: {}%, range: {}km".format(soc, remaining_range))
+    logging.info("Zoe API: soc: {}%, range: {}km, charging status: {}, plug status: {}".format(soc, remaining_range, charging_status, plug_status))
 
     return soc, remaining_range, charging_status, plug_status
 
 
-def update(gigya):
-    soc, remaining_range, charging_status, plug_status = getSocRange(gigya)
-
+def update_mqtt():
     zoe = {'soc': soc, 'range': remaining_range}
-
     mqttc.publish(topic=ZOE_MQTT_PREFIX, payload=json.dumps(zoe), qos=0, retain=True)
-
-    return charging_status
 
 
 if __name__ == '__main__':
@@ -66,15 +66,20 @@ if __name__ == '__main__':
     logging.info("Connected to {}:{}".format(BROKER_HOST, BROKER_PORT))
 
     exception_counter = 0
+    next_update_ts = time.time()
 
     mqttc.loop_start()
     while True:
         try:
-            charging_status = update(g)
-            if (charging_status in _ACTIVE_STATES):
-                time.sleep(FREQUENCY_ACTIVE)
-            else:
-                time.sleep(FREQUENCY_INACTIVE)
+            if (time.time() >= next_update_ts):
+                soc, remaining_range, charging_status, plug_status = getSocRange(g)
+                if (charging_status in _ACTIVE_STATES):
+                    next_update_ts = time.time() + FREQUENCY_ACTIVE
+                else:
+                    next_update_ts = time.time() + FREQUENCY_INACTIVE
+            if (soc != None):
+                update_mqtt()
+            time.sleep(MQTT_FREQUENCY)
         except KeyboardInterrupt:
             logging.warning("Keyboard interruption")
             break
